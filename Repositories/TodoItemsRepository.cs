95facc576454702e3b302c84ad196716fa5b33da
Kent Tristan Yves Sarmiento <kentsarmiento@gmail.com>
2022-01-20 11:32:01 +0800

    Initial repository support

---
+++ Repositories/TodoItemsRepository.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using ASPNetCore5TodoAPI.Models;
+using ASPNetCore5TodoAPI.Datastores;
+
+namespace ASPNetCore5TodoAPI.Repositories
+{
+    public class TodoItemsRepository : ITodoItemsRepository
+    {
+        private readonly ITodoItemsDatastore _todoItemsDatastore;
+
+        public TodoItemsRepository(ITodoItemsDatastore todoItemsDatastore)
+        {
+            _todoItemsDatastore = todoItemsDatastore;
+        }
+
+        public List<TodoItem> Get() =>
+            _todoItemsDatastore.Get();
+
+        public TodoItem Get(string id) =>
+            _todoItemsDatastore.Get(id);
+
+        public TodoItem Create(TodoItem item) =>
+            _todoItemsDatastore.Create(item);
+    }
+}

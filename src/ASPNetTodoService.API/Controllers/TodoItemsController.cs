97aae6da3c67750214762d34cb587bb4f50dd24b
Kent Tristan Yves Sarmiento <kentsarmiento@gmail.com>
2022-02-05 15:59:38 +0800

    Remove unused references

--- src/ASPNetTodoService.API/Controllers/TodoItemsController.cs
+++ src/ASPNetTodoService.API/Controllers/TodoItemsController.cs
@@ -1,7 +1,5 @@
-﻿using System;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
 using System.Linq;
-using System.Threading.Tasks;
 using System.Net.Mime;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
--- src/ASPNetTodoService.API/DTOs/TodoItemDTO.cs
+++ src/ASPNetTodoService.API/DTOs/TodoItemDTO.cs
@@ -1,9 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-
-namespace ASPNetTodoService.API.DTOs
+﻿namespace ASPNetTodoService.API.DTOs
 {
     public class TodoItemDTO
     {

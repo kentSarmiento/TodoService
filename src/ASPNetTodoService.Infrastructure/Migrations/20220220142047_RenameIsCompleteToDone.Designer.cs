40ba80c28f7e50c64e31a4fa6b7779183813a906
Kent Tristan Yves Sarmiento <kentsarmiento@gmail.com>
2022-02-20 22:30:58 +0800

    First Migration for changed schema

--- src/ASPNetTodoService.Infrastructure/Migrations/20220220142047_RenameIsCompleteToDone.Designer.cs
+++ src/ASPNetTodoService.Infrastructure/Migrations/20220220142047_RenameIsCompleteToDone.Designer.cs
@@ -2,14 +2,16 @@
 using ASPNetTodoService.Infrastructure.Repositories;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 namespace ASPNetTodoService.Infrastructure.Migrations
 {
     [DbContext(typeof(TodoContext))]
-    partial class TodoContextModelSnapshot : ModelSnapshot
+    [Migration("20220220142047_RenameIsCompleteToDone")]
+    partial class RenameIsCompleteToDone
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
